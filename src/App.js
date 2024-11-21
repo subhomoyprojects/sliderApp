211bcf3df169dc95c0c7436bcffb18296aa74d97
webskitters <webskitters@webskitterss-iMac-Pro.local>
2024-11-21 19:57:01 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -2,10 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let moveLeft = () => { }
+  let moveRight = () => { }
   return (
     <>
-      <h1>My name is Subhomoy</h1>
-
+      <h1>Slider</h1>
+      <div className="sliderWrapper">
+        <div className='slider'>
+          <div className="sliderItem">1</div>
+          <div className="sliderItem">2</div>
+          <div className="sliderItem">3</div>
+        </div>
+        <div className="btnHolder">
+          <button type='button' onClick={moveLeft} className='prevSlider'>prev</button>
+          <button type='button' onClick={moveRight} className='nextSlider'>next</button>
+        </div>
+      </div>
     </>
   );
 }
